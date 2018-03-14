b7b2dca9b49ef5aa74202de6be1e45c28aa94ed9
Sudhanshu Siddh <ssiddh@iu.edu>
2018-03-14 06:43:07 -0400

    Conditional practice exercise

---
+++ Start2/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="selectedComponent = 'appBlue', quoteData='Illo Illo'">Load Blue Template</button>
+                <button class="btn btn-success" @click="selectedComponent = 'appGreen', quoteData='Illo Ji Sanam'">Load Green Template</button>
+                <button class="btn btn-danger" @click="selectedComponent = 'appRed', quoteData='Hum Aa Gaye'">Load Red Template</button>
+                <hr>
+                <component :is="selectedComponent">
+                    <p>{{quoteData}}</p>
+                </component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data: function(){
+            return {
+                selectedComponent: 'appBlue',
+                quoteData: 'Illo Illo'
+            };
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
+</script>
+
+<style>
+</style>
