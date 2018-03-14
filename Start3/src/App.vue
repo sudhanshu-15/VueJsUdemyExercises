38b1b0b48cfa81d09b07dc5ae7adb5cda8e1390c
Sudhanshu Siddh <ssiddh@iu.edu>
2018-03-14 06:52:54 -0400

    QuotesAssignment

---
+++ Start3/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <app-header :quote-counts="quotesArr.length" :max-quotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="updateQuoteArray"></app-new-quote>
+        <app-quote-grid :quotes="quotesArr" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import NewQuote from './components/NewQuote.vue';
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import Header from './components/Header.vue';
+    export default {
+        data: function() {
+            return {
+                maxQuotes: 15,
+                quotesArr: [
+                    'To be or not to be'
+                ]
+            }
+        },
+        components: {
+            appQuoteGrid : QuoteGrid,
+            appNewQuote : NewQuote,
+            appHeader : Header
+        },
+        methods: {
+            updateQuoteArray(quote) {
+                if(this.quotesArr.length >= this.maxQuotes){
+                    return alert('Please delete Quotes first!');
+                }
+                if(quote){
+                    this.quotesArr.push(quote);
+                }else{
+                    return alert('Can not add empty quotes');
+                }
+                
+            },
+            deleteQuote(index) {
+                this.quotesArr.splice(index, 1);
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ Start3/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row">
+        <div class="col-sm-12">
+            <h3>Quotes Added</h3>
+            <div class="progress">
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"
+                    :style="{width: (quoteCounts / maxQuotes) * 100 + '%'}">
+                        {{quoteCounts}} / {{maxQuotes}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['quoteCounts', 'maxQuotes']
+    }
+</script>
